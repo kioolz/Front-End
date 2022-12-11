07c6ac3df8501c5ef17a07168e3f2b965f7c7940
Kioolz <caio.vorb@yahoo.com.br>
2022-12-11 18:20:47 -0300

    Create tutorial005.js

---
+++ Curso - Desenvolvimento Web - Rocket Seat/tutorial005.js
@@ -0,0 +1,22 @@
+// Operadores de lógica
+//&& "E" As duas condições devem ser verdadeiras para que a condição final seja verdadeira.
+//"OU" Uma das condições deve ser verdadeira para que a condição final seja verdadeira.
+//"NÃO" Nega uma condição.
+console.log(5 == 5 && 6 == 6) // true
+console.log(5 == 5 && 6 != 6) // false
+console.log(5 != 5 || 6 == 6) //true
+console.log(5 == 5 || 6 != 6) // true
+console.log(!(5 > 6)) // true
+console.log(!(5 < 6)) //false
+    // Operadores de comparação
+    /* =============================================================
+    > Maior
+    < Menor
+    >= Maior igual a
+    <= Menor igual a 
+    == Igual a
+    === Igual e do mesmo tipo 
+    != Diferente de
+    !== Diferente, inclusive do tipo
+    */
+    /
