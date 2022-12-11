dad60b468a541d88606ac519b1892d8c5996c200
Kioolz <caio.vorb@yahoo.com.br>
2022-12-11 18:20:47 -0300

    Create tutorial006.js

---
+++ Curso - Desenvolvimento Web - Rocket Seat/tutorial006.js
@@ -0,0 +1,29 @@
+// Criar um programa que calcula a média
+// das notas entre os alunos e envia
+// mensagem do cálculo da média 
+// Usando arrays e vetores
+
+// Eu posso criar um objeto chamado alunos, onde posso colocar os valores dentro 
+// das propriedades nome e nota
+const alunos = [{
+        nome: "Mayk",
+        nota: 9.8
+    },
+    {
+        nome: "Diego",
+        nota: 10
+
+    },
+    {
+        nome: "Fulano",
+        nota: 2
+
+    }
+]
+
+//Operação usando um array
+const media = (alunos.[0].nota + alunos[1].nota + alunos.[2].nota)
+
+//Outro tipo de array
+const nomesdeAlunos = ["Mayk", "Diego", "Fulano"]
+console.log(alunos[1])
