e62e527c74fe73fb580d6b577b18c9e3d06fc5d1
Kioolz <caio.vorb@yahoo.com.br>
2022-12-11 18:20:44 -0300

    Create tutorial001.js

---
+++ Curso - Desenvolvimento Web - Rocket Seat/tutorial001.js
@@ -0,0 +1,10 @@
+//O Curso do gustavo guanabara me ensinou como fazer os comentários em JS
+// Isso me lembra até que um exercício interessante que ele passou é
+// Como diferenciar os comandos de comentário em JS, CSS e HTML.
+
+
+//Criar uma variável
+const nome = 'Caio'
+    // O comando console.log() faz 
+    //com que as informações sejam exibidas no terminal
+console.log(nome)
