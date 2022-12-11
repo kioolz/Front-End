bb1d93c88bfb85aaeeaa2572057aa9698f1166a7
Kioolz <caio.vorb@yahoo.com.br>
2022-12-11 20:58:12 -0300

    Create js006 - JS cliente side.js

---
+++ JS/js006 - JS cliente side.js
@@ -0,0 +1,15 @@
+/* Este é um resumo básico das técnicas de programação do lado do cliente */
+
+/* Incorporando JS em um arquivo html */
+
+<html>
+    <head>
+        <script src="library.js"></script>
+    </head>
+
+    <body>
+        <p>Parágrafo em HTML</p>
+        <script> {/* E este é um código JS do lado do cliente incorporado por um arqiuvo HTML */}</script>
+        
+    </body>
+</html>
