b9db774e6c851441be88f951c233236b05e7fc6b
Kioolz <caio.vorb@yahoo.com.br>
2022-12-11 18:20:45 -0300

    Create tutorial003.js

---
+++ Curso - Desenvolvimento Web - Rocket Seat/tutorial003.js
@@ -0,0 +1,23 @@
+//Voltando ao programa anterior, que calcula a média das notas dos alunos
+
+
+const aluno01 = 'mayk'
+const aluno02 = 'Diego'
+const aluno03 = 'Fulano'
+
+const notaAluno01 = 9.8
+const notaAluno02 = 10
+const notaAluno03 = 4
+
+
+//Usando variáveis definidas pelo usuário direto na variável
+const somasimples = 1 + 512341
+    //Exibindo 
+console.log(somasimples) //Resultado da operação no terminal
+console.log(typeof somasimples) //Exibe o tipo de variável gerado pela operação
+
+
+//Formula de média simples. Soma-se todas as notas e divide pelo número de alunos
+const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
+    //Exibe o resultado no terminal
+console.log(media)
