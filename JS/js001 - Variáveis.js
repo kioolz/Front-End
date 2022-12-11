5889081752cfea6a8ff8e696fda4650ba0c065e4
Kioolz <caio.vorb@yahoo.com.br>
2022-12-11 20:58:08 -0300

    Create js001 - Variáveis.js

---
+++ JS/js001 - Variáveis.js
@@ -0,0 +1,23 @@
+// Tudo o que vem após barras normais duplas é um comentário em linguagem natural
+
+//Números inteiros e reais
+
+x = 1; // Números
+x = 0.01; // Número float
+
+//Strings
+
+x = "hello world"; // Strings de texto entre aspas
+x = 'JavaScript' // Apóstrofos tambem podem delimitar strings
+
+
+// Variáveis booleanas
+
+x = true; // Verdadeiro
+x = false; // falso
+
+// Variável nula e variável indefinida
+
+x = null; // X é um valor especial que significa "nenhum valor" ou valor nulo.
+x = undefined; // "Indefinido é como nulo"
+
