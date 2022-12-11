15091d42d5fc24f1a234bffc7741e7f560509026
Kioolz <caio.vorb@yahoo.com.br>
2022-12-11 20:58:09 -0300

    Create js003 - Operadores lógicos.js

---
+++ JS/js003 - Operadores lógicos.js
@@ -0,0 +1,49 @@
+
+// Operadores básicos
+
+3 + 2 // Soma
+3 - 2 // Subtração
+3 * 2 // Multiplicação
+3 / 2 // Divisão
+points[1].x - points[0].x // Operandos que dependem de x
+"3" + "2" // concatenação de strings
+
+
+//  operadores aritméticos 
+
+var count = 0; /* Define uma variável */
+count++; /* Incrementa a variável */
+count--; /* Decrementa a variável */
+count +=2; /* Soma 2 - Análogo a count = count + 2 */
+count *=3; /* Multiplica por 3 - Análogo a count = count * 3 */
+count /* 6 - nomes de variáveis também são expressos */
+
+// Operações de igualdade e relacionais
+
+var x = 2, y = 3; //Definido variáveis para testes
+
+x == y /* Igualdade ; false*/
+x != y /* desigualdade ; true */
+
+x < y /* menor que ; true */
+x <= y/* menor ou igual a, verdadeiro */
+x > y /* falso, maior que */
+x => y /* falso, maior ou igual a */
+
+"two" == "three" /* falso, as duas strings são diferentes */
+
+"two" > "three" /* verdadeiro, "tw" é alfabeticamente maior que th */
+
+false == (x > y) /* verdadeiro, falso é igual a falso */
+
+
+/* /Os operadores lógicos combinam ou invertem valores booleanos */
+
+(x == 2) & ( y == 3) // true, as duas compoarações são verdadeiras. (& == E)
+
+(x > 3) || (y < 3) /* false, nenhuma das comparações é verdadeira. (|| == OU) */
+
+!(x == y) /* true, ! inverte um valor booleano */
+
+
+
